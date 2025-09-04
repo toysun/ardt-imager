618de46e7ae8f31abe9f694fa7360a5de0999d24
toysun <52024437+toysun@users.noreply.github.com>
2025-09-04 09:48:35 +0900

    Add files via upload

---
+++ playvideo.js
@@ -0,0 +1,69 @@
+var _0x2299 = [
+  "bind",
+  "1nbkzkc",
+  "play",
+  "addEventListener",
+  "removeEventListener",
+  "4191xVYqlr",
+  "269004PCxEly",
+  "registerComponent",
+  "54729baUHGc",
+  "849294JCTMCn",
+  "39gJhkWJ",
+  "#myFooter",
+  "click",
+  "277465nzAXpG",
+  "864889mEofVp",
+  "playvideo",
+  "645147CoLvrl",
+  "2gBUnJK",
+  "onClick",
+  "setAttribute",
+];
+var _0x32c1 = function (_0x4ec5f5, _0x1faecc) {
+  _0x4ec5f5 = _0x4ec5f5 - 0x140;
+  var _0x22990f = _0x2299[_0x4ec5f5];
+  return _0x22990f;
+};
+var _0x3cb467 = _0x32c1;
+(function (_0x195014, _0x198d6b) {
+  var _0x3d0343 = _0x32c1;
+  while (!![]) {
+    try {
+      var _0x2ef017 =
+        parseInt(_0x3d0343(0x151)) +
+        -parseInt(_0x3d0343(0x153)) +
+        parseInt(_0x3d0343(0x145)) * -parseInt(_0x3d0343(0x14c)) +
+        parseInt(_0x3d0343(0x140)) +
+        parseInt(_0x3d0343(0x147)) +
+        -parseInt(_0x3d0343(0x150)) * -parseInt(_0x3d0343(0x141)) +
+        -parseInt(_0x3d0343(0x144)) * parseInt(_0x3d0343(0x148));
+      if (_0x2ef017 === _0x198d6b) break;
+      else _0x195014["push"](_0x195014["shift"]());
+    } catch (_0x17c001) {
+      _0x195014["push"](_0x195014["shift"]());
+    }
+  }
+})(_0x2299, 0x6e6fa),
+  AFRAME[_0x3cb467(0x152)](_0x3cb467(0x146), {
+    init: function () {
+      var _0x2f79b5 = _0x3cb467;
+      this[_0x2f79b5(0x149)] = this[_0x2f79b5(0x149)][_0x2f79b5(0x14b)](this);
+    },
+    play: function () {
+      var _0x511e50 = _0x3cb467;
+      window[_0x511e50(0x14e)](_0x511e50(0x143), this[_0x511e50(0x149)]);
+    },
+    pause: function () {
+      var _0x1b2bd1 = _0x3cb467;
+      window[_0x1b2bd1(0x14f)]("click", this[_0x1b2bd1(0x149)]);
+    },
+    onClick: function (_0x24ed8f) {
+      var _0x1e4b2c = _0x3cb467,
+        _0x4fea25 = document["querySelector"]("#vid"),
+        _0x4d0b07 = document["querySelector"](_0x1e4b2c(0x142));
+      if (!_0x4fea25) return;
+      _0x4d0b07[_0x1e4b2c(0x14a)]("visible", "false"),
+        _0x4fea25[_0x1e4b2c(0x14d)]();
+    },
+  });
